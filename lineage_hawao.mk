aa5078b9a2ce7f03b816cf2b5492878387295ff5
ZetLink <royalezetlink@gmail.com>
2025-02-17 17:47:16 -0300

    hawao: Remove RisingOS stuff

--- lineage_hawao.mk
+++ lineage_hawao.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device
 $(call inherit-product, device/motorola/hawao/device.mk)
 
-# Inherit some common RisingOS stuff.
+# Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
@@ -20,18 +20,9 @@ PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g42
 PRODUCT_MANUFACTURER := motorola
 
-# RisingFlags
-TARGET_ENABLE_BLUR := false
-RISING_BATTERY := 5000mah
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
-
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    RisingChipset="Snapdragon 680" \
-    RisingMaintainer="ZetLink" \
     BuildDesc="hawao_g-user 13 T2SES33.73-23-2-12 f73e35-2bfcf release-keys" \
     BuildFingerprint=motorola/hawao_g/hawao:13/T2SES33.73-23-2-12/f73e35-2bfcf:user/release-keys \
     DeviceProduct=hawao_g
